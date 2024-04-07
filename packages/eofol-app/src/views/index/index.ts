540fa0d3d5ca8e6ed3388a293d95a8bae2f475c5
wurducius <wurducius@gmail.com>
2024-04-07 14:48:06 +0200

    Map keyboard keys

--- packages/eofol-app/src/views/index/index.ts
+++ packages/eofol-app/src/views/index/index.ts
@@ -238,14 +238,65 @@ defineBuiltinElement({
     scaleLength: getScaleLength(defaultScale),
   },
   render: (state, setState) => {
+    // @ts-ignore
+    const freq = state.freq;
+
     document.onkeydown = (event) => {
-      // @ts-ignore
-      handleKeyDown(event, state.freq, "z", 24);
+      handleKeyDown(event, freq, "z", 0);
+      handleKeyDown(event, freq, "x", 1);
+      handleKeyDown(event, freq, "c", 2);
+      handleKeyDown(event, freq, "v", 3);
+      handleKeyDown(event, freq, "b", 4);
+      handleKeyDown(event, freq, "n", 5);
+      handleKeyDown(event, freq, "m", 6);
+      handleKeyDown(event, freq, "a", 7);
+      handleKeyDown(event, freq, "s", 8);
+      handleKeyDown(event, freq, "d", 9);
+      handleKeyDown(event, freq, "f", 10);
+      handleKeyDown(event, freq, "g", 11);
+      handleKeyDown(event, freq, "h", 12);
+      handleKeyDown(event, freq, "j", 13);
+      handleKeyDown(event, freq, "k", 14);
+      handleKeyDown(event, freq, "l", 15);
+      handleKeyDown(event, freq, "q", 16);
+      handleKeyDown(event, freq, "w", 17);
+      handleKeyDown(event, freq, "e", 18);
+      handleKeyDown(event, freq, "r", 19);
+      handleKeyDown(event, freq, "t", 20);
+      handleKeyDown(event, freq, "y", 21);
+      handleKeyDown(event, freq, "u", 22);
+      handleKeyDown(event, freq, "i", 23);
+      handleKeyDown(event, freq, "o", 24);
+      handleKeyDown(event, freq, "p", 25);
     };
 
     document.onkeyup = (event) => {
-      // @ts-ignore
-      handleKeyUp(event, state.freq, "z", 24);
+      handleKeyUp(event, freq, "z", 0);
+      handleKeyUp(event, freq, "x", 1);
+      handleKeyUp(event, freq, "c", 2);
+      handleKeyUp(event, freq, "v", 3);
+      handleKeyUp(event, freq, "b", 4);
+      handleKeyUp(event, freq, "n", 5);
+      handleKeyUp(event, freq, "m", 6);
+      handleKeyUp(event, freq, "a", 7);
+      handleKeyUp(event, freq, "s", 8);
+      handleKeyUp(event, freq, "d", 9);
+      handleKeyUp(event, freq, "f", 10);
+      handleKeyUp(event, freq, "g", 11);
+      handleKeyUp(event, freq, "h", 12);
+      handleKeyUp(event, freq, "j", 13);
+      handleKeyUp(event, freq, "k", 14);
+      handleKeyUp(event, freq, "l", 15);
+      handleKeyUp(event, freq, "q", 16);
+      handleKeyUp(event, freq, "w", 17);
+      handleKeyUp(event, freq, "e", 18);
+      handleKeyUp(event, freq, "r", 19);
+      handleKeyUp(event, freq, "t", 20);
+      handleKeyUp(event, freq, "y", 21);
+      handleKeyUp(event, freq, "u", 22);
+      handleKeyUp(event, freq, "i", 23);
+      handleKeyUp(event, freq, "o", 24);
+      handleKeyUp(event, freq, "p", 25);
     };
 
     return createElement("div", undefined, [
