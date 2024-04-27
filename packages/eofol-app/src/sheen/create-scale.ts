0d296237f85dc87c27b28986e973bf950e8c22f9
wurducius <wurducius@gmail.com>
2024-04-27 18:08:14 +0200

    Refactor createScale

--- packages/eofol-app/src/sheen/create-scale.ts
+++ packages/eofol-app/src/sheen/create-scale.ts
@@ -1,50 +1,31 @@
 import { FiddleState } from "../types";
-import { mod, onlyUnique } from "../util";
+import { mod } from "../util";
 import { normalizePeriod } from "./sheen";
+import { outputScale } from "./sheen-util";
 
-export const linearScale = (state: FiddleState, T: number, g: number) =>
-  Array.from({ length: T })
-    .map((item, index) => {
-      // @ts-ignore
-      const centPeriod = 1200 * Math.log2(state.tuning.period);
-      const val = mod(index * g, centPeriod);
-      return val === 0
-        ? // @ts-ignore
-          centPeriod.toFixed(state.options.decimalDigitsCent)
-        : // @ts-ignore
-          val.toFixed(state.options.decimalDigitsCent);
-    })
-    .sort((a, b) => Number(a) - Number(b))
-    .map((tone) => (tone.includes(".") ? tone : tone + "."))
-    .filter(onlyUnique)
-    .join("\n");
+export const linearScale = (T: number, g: number) =>
+  Array.from({ length: T }).map((item, index) => index * g);
 
-export const createEdo = (
-  state: FiddleState,
-  N: number // @ts-ignore
-) => linearScale(state, N, (1200 * Math.log2(state.tuning.period)) / N);
+export const createEdo = (state: FiddleState, N: number) =>
+  outputScale(
+    state, // @ts-ignore
+    linearScale(N, (1200 * Math.log2(state.tuning.period)) / N)
+  );
 
 export const createMOS = (state: FiddleState, N: number, T: number) => {
-  const vals = Array.from({ length: T })
-    .map((item, i) => {
-      // @ts-ignore
-      const periodCent = 1200 * Math.log2(state.tuning.period);
-      const val = mod(
-        (Math.floor(((i + 1) * N) / T) * periodCent) / N,
-        periodCent
-      );
-      return val === 0 ? periodCent : val;
-    }) // @ts-ignore
-    .map((tone) => tone.toFixed(state.options.decimalDigitsCent))
-    .map((tone) => (tone.includes(".") ? tone : tone + "."));
-  return vals.join("\n");
+  const vals = Array.from({ length: T }).map((item, i) => {
+    // @ts-ignore
+    const periodCent = 1200 * Math.log2(state.tuning.period);
+    return mod((Math.floor(((i + 1) * N) / T) * periodCent) / N, periodCent);
+  });
+  return outputScale(state, vals);
 };
 
 export const createLinear = (state: FiddleState, T: number, g: number) =>
-  linearScale(state, T, g);
+  outputScale(state, linearScale(T, g));
 
 export const createMeantone = (state: FiddleState, T: number, comma: number) =>
-  linearScale(state, T, 1200 * Math.log2(Math.pow(5, 1 / comma)));
+  outputScale(state, linearScale(T, 1200 * Math.log2(Math.pow(5, 1 / comma))));
 
 export const createHarmonicSeries = (state: FiddleState, T: number) => {
   const vals = [0];
@@ -56,14 +37,7 @@ export const createHarmonicSeries = (state: FiddleState, T: number) => {
       1200 * Math.log2(normalizePeriod(1 / i, state.tuning.period))
     );
   }
-  return (
-    vals
-      .sort((a, b) => a - b)
-      // @ts-ignore
-      .map((val) => val.toFixed(state.options.decimalDigitsCent))
-      .filter(onlyUnique)
-      .join("\n")
-  );
+  return outputScale(state, vals);
 };
 
 export const createJust = (state: FiddleState, T: number, limit: number) => {
@@ -86,14 +60,7 @@ export const createJust = (state: FiddleState, T: number, limit: number) => {
     );
   }
 
-  return (
-    vals
-      .sort((a, b) => a - b)
-      // @ts-ignore
-      .map((val) => val.toFixed(state.options.decimalDigitsCent))
-      .filter(onlyUnique)
-      .join("\n")
-  );
+  return outputScale(state, vals);
 };
 
 export const createRatioChord = (state: FiddleState, chord: string) => {
@@ -104,17 +71,13 @@ export const createRatioChord = (state: FiddleState, chord: string) => {
     result[i - 1] = Number(points[i]) / first;
   }
   result[points.length - 1] = first;
-  return result
-    .map(
-      (tone) =>
-        1200 *
-        Math.log2(
-          // @ts-ignore
-          normalizePeriod(tone, state.tuning.period)
-        )
-    )
-    .sort((a, b) => a - b) // @ts-ignore
-    .map((tone) => tone.toFixed(state.options.decimalDigitsCent))
-    .filter(onlyUnique)
-    .join("\n");
+  const vals = result.map(
+    (tone) =>
+      1200 *
+      Math.log2(
+        // @ts-ignore
+        normalizePeriod(tone, state.tuning.period)
+      )
+  );
+  return outputScale(state, vals);
 };
