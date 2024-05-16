5665c38743ee58635ed1277a21787e7822b794c6
wurducius <wurducius@gmail.com>
2024-05-16 22:48:16 +0200

    Fix build

--- packages/eofol-app/src/views/index/views/index.ts
+++ packages/eofol-app/src/views/index/views/index.ts
@@ -1,7 +1,6 @@
 export * from "./about";
 export * from "./analyze";
 export * from "./appbar";
-export * from "./docs";
 export * from "./options";
 export * from "./scale";
 export * from "./synth";
